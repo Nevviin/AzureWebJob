18a444d189265ac7eefbf05ac4aef6d6f6224ca2
Nevin <nevviin@gmail.com>
2021-11-13 11:47:27 +1100

    pushing  Azure Web Job which can run as a bacth job on specified time

---
+++ AzureWebJob/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace AzureWebJob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"AzureWebJob Ran at {DateTime.Now.ToString()}");
+
+            File.AppendAllText("RunLog.txt", DateTime.Now.ToString() + Environment.NewLine);
+            
+            
+            
+        }
+    }
+}
